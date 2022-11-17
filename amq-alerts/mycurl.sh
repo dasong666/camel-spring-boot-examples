c70ceff0bd5241724de7ee8c6e72a1484068430e
David Song <95264227+dasong666@users.noreply.github.com>
2022-11-17 08:39:41 -0800

    Update mycurl.sh

--- amq-alerts/mycurl.sh
+++ amq-alerts/mycurl.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 #broker metrics
-curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\"/TotalMessageCount
+curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\"/TotalMessageCount | jq '.request.attribute,.value'
 
-curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\"/ConnectionCount
+curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\"/ConnectionCount | jq '.request.attribute,.value'
 
-curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/java.lang:type=Threading/PeakThreadCount
+curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/java.lang:type=Threading/PeakThreadCount | jq '.request.attribute,.value' 
 
-curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\",component=addresses,address=\"JB440web\",subcomponent=queues,routing-type=\"multicast\",queue=\"JB440Queue\"/MessageCount
+#curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\",component=addresses,address=\"JB440web\",subcomponent=queues,routing-type=\"multicast\",queue=\"JB440Queue\"/MessageCount
 
-#curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\"/AddressMemoryUsagePercentage
+curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\"/AddressMemoryUsagePercentage
 
-#curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\"/DiskStoreUsage
+curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/org.apache.activemq.artemis:broker=\"0.0.0.0\"/DiskStoreUsage | jq '.request.attribute,.value'
 
 #JVM metrics
-#curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/java.lang:type=Memory/HeapMemoryUsage
+curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/java.lang:type=Memory/HeapMemoryUsage | jq '.request.attribute,.value'
 
-#curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/java.lang:type=OperatingSystem/ProcessCpuLoad
+#curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/java.lang:type=Memory/HeapMemoryUsage 
+
+curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/java.lang:type=OperatingSystem/ProcessCpuLoad | jq '.request.attribute,.value'
 
 #curl -L -H "Origin: http://localhost" -u test:catdog123 http://192.168.0.18:8161/console/jolokia/read/java.lang:type=GarbageCollector,name=Copy/LastGcInfo
