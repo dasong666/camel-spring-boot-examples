0edad903402a039f0f7fed4bdfdadc8967f90c79
joelicious <joe@joelicio.us>
2022-11-22 23:07:43 -0500

    Reorganizing projects and using RH supported dependencies.

--- amq-alerts/src/main/java/com/redhat/consulting/monitoring/RestApplication.java
+++ amq-alerts/src/main/java/com/redhat/consulting/monitoring/RestApplication.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sample.camel;
+package com.redhat.consulting.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
